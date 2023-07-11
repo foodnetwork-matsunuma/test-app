3de8dd8d8d38e49cb5f3f8a73425dbabe3871314
foodnetwork-matsunuma <48036828+foodnetwork-matsunuma@users.noreply.github.com>
2023-07-11 13:55:01 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,12 @@
-const shopDomain = 'suehiro.jp'
-
-// Shopify
-const shopifyApiVer = '2023-04' // ※3ヶ月毎に更新
-const shopifyDomain = 'check.' + shopDomain
-const shopifyUrl = 'https://' + shopifyDomain
-const shopifyApiUrl = shopifyUrl + '/api/' + shopifyApiVer + '/graphql.json'
-const shopifyApiHeaders = {
-  Accept: 'application/json',
-  'Content-Type': 'application/graphql',
-  'X-Shopify-Storefront-Access-Token': process.env.ACCESS_TOKEN,
-}
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   runtimeConfig: {
     // Shopify
-    SHOPIFY_API_URL: shopifyApiUrl,
-    SHOPIFY_API_HEADERS: shopifyApiHeaders
+    SHOPIFY_API_URL: process.env.URL, //shopifyApiUrl,
+    SHOPIFY_API_HEADERS: {
+      Accept: 'application/json',
+      'Content-Type': 'application/graphql',
+      'X-Shopify-Storefront-Access-Token': process.env.ACCESS_TOKEN,
+    }
   },
 })
