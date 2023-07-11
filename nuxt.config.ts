3b52e8e6518df86b178e3c034c6f744c357b5e19
foodnetwork-matsunuma <48036828+foodnetwork-matsunuma@users.noreply.github.com>
2023-07-11 13:45:14 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,10 @@ const shopifyApiVer = '2023-04' // ※3ヶ月毎に更新
 const shopifyDomain = 'check.' + shopDomain
 const shopifyUrl = 'https://' + shopifyDomain
 const shopifyApiUrl = shopifyUrl + '/api/' + shopifyApiVer + '/graphql.json'
-const shopifyStoreFrontAccessToken = '88dfccbf288c8c539e65bd8ec617b720'
 const shopifyApiHeaders = {
   Accept: 'application/json',
   'Content-Type': 'application/graphql',
-  'X-Shopify-Storefront-Access-Token': shopifyStoreFrontAccessToken,
+  'X-Shopify-Storefront-Access-Token': ACCESS_TOKEN,
 }
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
